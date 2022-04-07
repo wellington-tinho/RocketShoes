4208f4819f0244c6a300841e855cf587b1cb4544
Wellington R <sousa_wellington@outlook.com>
2022-04-06 22:05:56 -0300

    fixbug: corret api for development

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
+var API
+if (process.env.NODE_ENV !== 'production') {
+  baseURL = 'http://localhost:3333'
+  
+} else {
+  baseURL = ('https://rocketshoes.luizbatanero.vercel.app/products')
+  
+}
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
-});
+  baseURL: API,
+})
+
+api.get('/')
+.then(response => console.info(response.data))
+.catch((err) => console.warn('Não foi possivel se conectar com a API, \n\n',err)
+)
